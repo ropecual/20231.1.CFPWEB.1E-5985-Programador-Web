7505b8c0e2b92c70d2349ced5305c2c8356f6af2
Roni P. Alvarenga Ferraresso <rope.alvarenga@gmail.com>
2023-10-03 10:52:48 -0300

    Aula JS

---
+++ javascript/listas/script.js
@@ -0,0 +1,46 @@
+var numeros = [0, 2, 5, 232]
+var mistas = ['nome', 4, "er", 5.6]
+
+mistas[0] = 'Roni'
+
+
+let frutas = ['maca', 'pera', 'uva', 'abacate', 'limao', 'abacaxi']
+
+
+// adicionando no final da lista
+frutas.push('banana')
+console.log(frutas)
+
+// removendo um item do final da lista
+retorno = frutas.pop() // ele, alem de remover o ultimo elemente, ele o retorna, podendo salvar em uma variavel
+console.log(frutas)
+console.log(retorno)
+
+// adicionando no inicio da lista
+frutas.unshift('banana')
+console.log(frutas)
+
+// removendo um item do inicio da lista
+retorno = frutas.shift() // ele, alem de remover o primeiro elemente, ele o retorna, podendo salvar em uma variavel
+console.log(frutas)
+console.log(retorno)
+
+
+// remove um elemento do meio da lista (entre o primeiro e o ultimo) sendo o primeiro argumento
+// o primeiro elemento a ser removido e o segundo argumento a quantidade a serem removidos
+console.log(frutas)
+frutas.splice(2, 2)
+console.log(frutas)
+
+// adiciona ou substitui um elemento do meio da lista (entre o primeiro e o ultimo) sendo o primeiro argumento
+// o primeiro elemento a ser adicionado e o segundo argumento a quantidade a serem substituidos
+console.log(frutas)
+frutas.splice(2, 0, 'laranja', 'lima')
+console.log(frutas)
+
+
+// Percorrendo listas
+
+for (let indice = 0; indice > frutas.length; indice++) {
+    console.log(`A fruta n ${indice} eh a ${frutas[indice]}`)
+}
